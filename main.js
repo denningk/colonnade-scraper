1e5e03b4f540e5840e5709b8ecb2d03046037901
Keith Denning <keithdenning1@gmail.com>
2019-11-04 21:32:54 -0500

    Login and search

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 const puppeteer = require('puppeteer');
 
-const main = async () => {
-    const [browser, page] = await setUp();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
-
-    await teardown(browser);
-};
-
 const setUp = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: {width: 1920, height: 1080},
+        slowMo: 100
+    });
     const page = await browser.newPage();
 
     return [browser, page];
@@ -18,5 +14,29 @@ const setUp = async () => {
 const teardown = async (browser) => {
     await browser.close();
 }
+
+const login = async (page) => {
+    await page.goto('https://colonnadeconnections.wlu.edu/');
+    await page.type('#PC12224_txtUsername', 'kdenning');
+    await page.type('#PC12224_txtPassword', 'Keithjake1!');
+    await page.click('#PC12224_btnLogin');
+};
+
+const main = async () => {
+    const [browser, page] = await setUp();
+
+    await login(page);
+
+    await page.goto('https://colonnadeconnections.wlu.edu/directory/advanced-search');
+    await page.waitForSelector('#PC13914_ctl00_64_0');
+
+    await page.type('#PC13914_ctl00_59_0', '18');
+    await page.select('#PC13914_ctl00_64_0', 'Sigma Nu');
+    await page.click('#PC13914_ctl00_btnRefresh');
+    await page.waitForSelector('#PC13914_ctl00_directoryOutputGridView_myGridView');
+
+    await teardown(browser);
+};
+
 
 main();
